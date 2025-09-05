81a60609cbf007a778db3a21761d0223ff917bd9
lauirf <592184299@qq.com>
2025-09-05 19:39:19 +0800

    feat: add fastapi

--- enemy_manager.py
+++ enemy_manager.py
@@ -1,16 +1,45 @@
+import threading
+from fastapi import FastAPI
+import uvicorn
 from logging_config import get_logger
 
 
 logger = get_logger(__name__)
 
+
 class EnemyManager:
-    """网络数据包抓取器"""
+    """EnemyManager"""
+
     def __init__(self):
         self.logger = logger
         self.enemies = {}
+        self.app = FastAPI()
+        host="127.0.0.1"
+        port=1289
+
+        # 注册路由
+        @self.app.get("/enemies")
+        def list_enemies():
+            return self.enemies
+
+        @self.app.get("/enemies/{enemy_name}")
+        def get_enemy(enemy_name: str):
+            for enemy_uuid, enemy in self.enemies.items():
+                if enemy.get('name') == enemy_name:
+                    return enemy
+            return {}
+
+        # 后台启动 API
+        thread = threading.Thread(
+            target=lambda: uvicorn.run(
+                self.app, host=host, port=port, log_level="info"
+            ),
+            daemon=True
+        )
+        thread.start()
     
     def sync_enemy(self, id, name, hp, max_hp):
-        """同步敌人数据"""
+        """敌人管理器 + API 服务"""
         if not id:
             return
         enemy = self.enemies.get(id, {})
@@ -21,4 +50,5 @@ class EnemyManager:
         if max_hp:
             enemy['max_hp'] = max_hp
         self.enemies[id] = enemy
-        self.logger.info(f"同步敌人数据: {id} -> {enemy['name']}, HP: {enemy['hp']}/{enemy['max_hp']}")
+        if 'name' in enemy and 'hp' in enemy and 'max_hp' in enemy:
+            self.logger.info(f"同步敌人数据: {id} -> {enemy['name']}, HP: {enemy['hp']}/{enemy['max_hp']}")
